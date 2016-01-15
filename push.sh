388058c802a126e0274c3053376edd3c06df37d4
kevin <xxiiaass@sina.com>
2016-01-15 15:13:04 +0800

    auto commit

--- push.sh
+++ push.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-git checkout -b test
+git checkout -b $1
 git add --all
 git commit -m "auto commit"
-git push git@github.com:xxiiaass/MYCDN.git test
+git push git@github.com:xxiiaass/MYCDN.git $1
